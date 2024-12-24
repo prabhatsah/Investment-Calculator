968a40ce1e7b056d4c35a97b12729f0edfe6ee82
Prabhat Kumar <prabhatsah349@gmail.com>
2024-12-24 08:57:07 +0530

    UserInput.jsx added

--- Investment Calculator/src/components/UserInput.jsx
+++ Investment Calculator/src/components/UserInput.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export default function UserInput() {
   const [userInput, setUserInput] = useState({
     initialInvestment: 10000,
@@ -6,26 +8,57 @@ export default function UserInput() {
     duration: 10,
   });
 
-  function handleChange() {}
+  function handleChange(field, value) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [field]: value,
+      };
+    });
+  }
 
   return (
     <>
       <div id="user-input">
         <div id="input-group">
           <label>Initial Investment</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            onChange={(event) =>
+              handleChange("initialInvestment", event.target.value)
+            }
+            value={userInput.initialInvestment}
+          />
         </div>
         <div id="input-group">
           <label>Annual Investment</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            onChange={(event) =>
+              handleChange("annualInvestment", event.target.value)
+            }
+            value={userInput.annualInvestment}
+          />
         </div>
         <div id="input-group">
           <label>Return</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            onChange={(event) => handleChange("return", event.target.value)}
+            value={userInput.return}
+          />
         </div>
         <div id="input-group">
           <label>Duration</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            onChange={(event) => handleChange("duration", event.target.value)}
+            value={userInput.duration}
+          />
         </div>
       </div>
     </>
